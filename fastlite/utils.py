30ebf4c62f8cfc4eb02faecb9949a2c07ce2f3c6
Jeremy Howard <github@jhoward.fastmail.fm>
2024-06-04 13:08:49 +1000

    rm parsing

--- fastlite/utils.py
+++ fastlite/utils.py
@@ -11,7 +11,6 @@ import json
 import os
 import sys
 import json
-from dateutil import parser
 from typing import Dict, cast, BinaryIO, Iterable, Optional, Tuple, Type
 
 try:
@@ -482,55 +481,4 @@ def flatten(row: dict) -> dict:
 
 IGNORE = object()
 SET_NULL = object()
-
-
-def parsedate(value, dayfirst=False, yearfirst=False, errors=None):
-    """
-    Parse a date and convert it to ISO date format: yyyy-mm-dd
-    \b
-    - dayfirst=True: treat xx as the day in xx/yy/zz
-    - yearfirst=True: treat xx as the year in xx/yy/zz
-    - errors=r.IGNORE to ignore values that cannot be parsed
-    - errors=r.SET_NULL to set values that cannot be parsed to null
-    """
-    try:
-        return (
-            parser.parse(value, dayfirst=dayfirst, yearfirst=yearfirst)
-            .date()
-            .isoformat()
-        )
-    except parser.ParserError:
-        if errors is IGNORE:
-            return value
-        elif errors is SET_NULL:
-            return None
-        else:
-            raise
-
-
-def parsedatetime(value, dayfirst=False, yearfirst=False, errors=None):
-    """
-    Parse a datetime and convert it to ISO datetime format: yyyy-mm-ddTHH:MM:SS
-    \b
-    - dayfirst=True: treat xx as the day in xx/yy/zz
-    - yearfirst=True: treat xx as the year in xx/yy/zz
-    - errors=r.IGNORE to ignore values that cannot be parsed
-    - errors=r.SET_NULL to set values that cannot be parsed to null
-    """
-    try:
-        return parser.parse(value, dayfirst=dayfirst, yearfirst=yearfirst).isoformat()
-    except parser.ParserError:
-        if errors is IGNORE:
-            return value
-        elif errors is SET_NULL:
-            return None
-        else:
-            raise
-
-
-def jsonsplit(value, delimiter=",", type=str):
-    """
-    Convert a string like a,b,c into a JSON array ["a", "b", "c"]
-    """
-    return json.dumps([type(s.strip()) for s in value.split(delimiter)])
 
